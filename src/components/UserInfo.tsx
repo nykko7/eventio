f3031f054c0603b5dad07434206090c133e7db7a
Nykko <n.perezbus@gmail.com>
2023-09-04 18:38:57 -0300

    feat: clean up home page

---
+++ src/components/UserInfo.tsx
@@ -0,0 +1,42 @@
+import Link from "next/link"
+import { useCurrentUser } from "src/users/hooks/useCurrentUser"
+import logout from "src/auth/mutations/logout"
+import { useMutation } from "@blitzjs/rpc"
+import { Routes } from "@blitzjs/next"
+import styles from "src/styles/Home.module.css"
+
+export const UserInfo = () => {
+  const currentUser = useCurrentUser()
+  const [logoutMutation] = useMutation(logout)
+
+  if (currentUser) {
+    return (
+      <>
+        <button
+          className={styles.button}
+          onClick={async () => {
+            await logoutMutation()
+          }}
+        >
+          Logout
+        </button>
+        <div>
+          User id: <code>{currentUser.id}</code>
+          <br />
+          User role: <code>{currentUser.role}</code>
+        </div>
+      </>
+    )
+  } else {
+    return (
+      <>
+        <Link href={Routes.SignupPage()} className={styles.button}>
+          <strong>Sign Up</strong>
+        </Link>
+        <Link href={Routes.LoginPage()} className={styles.loginButton}>
+          <strong>Login</strong>
+        </Link>
+      </>
+    )
+  }
+}
---
+++ src/pages/about.tsx
@@ -0,0 +1,11 @@
+import { BlitzPage } from "@blitzjs/next"
+
+const Holi: BlitzPage = () => {
+  return (
+    <>
+      <div>holi holi</div>
+    </>
+  )
+}
+
+export default Holi
